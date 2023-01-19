f9159a8513276d1b29d3e8087949ed12fa78652d
MALLIKAARJUNM <arjun171ca0126@gmail.com>
2023-01-19 23:10:29 +0530

    (19-1)commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import RegistrationForm from './component/RegistrationForm.js'
--- src/component/RegistrationForm.js
+++ src/component/RegistrationForm.js
@@ -1,49 +1,50 @@
 import React, { useState } from 'react';
 import studiesOne from '../images/studiesOne.jpg'
+import { useEffect } from 'react';
 
 const RegistrationForm = () => {
 
-    const [student,setStudent] = useState({})
+    const [student,setStudent] = useState({uName:'',rollNum:'',emailID:'',mobileNum:0})
     const [userName,setUserName] = useState("");
     const [rollNo,setRollNo] = useState("");
     const [emailId,setEmailId] = useState("");
     const [mobileNo,setMobileNO] = useState(0);
 
-    const register = () => {
+    useEffect(()=>{
         setStudent({
-        userName : this.userName,
-        rollNo : this.rollNo,
-        emailId : this.emailId,
-        mobileNo : this.mobileNo
-        })
+            uName:userName,
+            rollNum:rollNo,
+            emailID:emailId,
+            mobileNum:mobileNo
+        });
         console.log(student);
-    }
+    },[userName,rollNo,emailId,mobileNo])
 
     return(
-        <div class='image' style={{backgroundImage:`url(${studiesOne})`}}>
-        <div class="ui inverted segment" style={{marginLeft:'35%', marginRight:'35%', marginTop:'5%'}}>
+        <div className='image' style={{backgroundImage:`url(${studiesOne})`}}>
+        <div className="ui inverted segment" style={{marginLeft:'35%', marginRight:'35%', marginTop:'5%'}}>
             <h1>Registration Form</h1>
-            <form class="ui inverted form">
+            <form className="ui inverted form">
                 <div>
-                    <div class="field">
+                    <div className="field">
                         <label>User Name</label>
-                            <input type="text" placeholder="user name" onChange={e=>setUserName(e.target.value)}/>
+                            <input type="text" placeholder="user name" onChange={(event)=>setUserName(event.target.value)} value={userName}/>
                     </div>
-                    <div class="field">
+                    <div className="field">
                         <label>Roll No</label>
-                            <input type="text" placeholder="roll no..." onChange={e=>setRollNo(e.target.value)}/>
+                            <input type="text" placeholder="roll no..." onChange={event=>setRollNo(event.target.value)} value={rollNo}/>
                     </div>
-                    <div class="field">
+                    <div className="field">
                         <label>Email Id</label>
-                            <input type="text" placeholder="email id..." onChange={e=>setEmailId(e.target.value)}/>
+                            <input type="text" placeholder="email id..." onChange={event=>setEmailId(event.target.value)} value={emailId}/>
                     </div>
-                    <div class="field">
+                    <div className="field">
                         <label>Mobile No</label>
-                            <input type="number" placeholder="mobile no..." onChange={e=>setMobileNO(e.target.value)}/>
+                            <input type="number" placeholder="mobile no..." onChange={event=>setMobileNO(event.target.value)} value={mobileNo}/>
                     </div>
                 </div>
-                <div class="field"><br/>
-                <button class="ui button" type="submit" style={{marginLeft:'35%', marginRight:'35%'}}>Register</button>
+                <div className="field"><br/>
+                <button className="ui button" type="submit" style={{marginLeft:'35%', marginRight:'35%'}}>Register</button>
                 </div>
             </form>
         </div>
