7896d92c03668514d0e82fd374d3e5921d5eced7
MALLIKAARJUNM <arjun171ca0126@gmail.com>
2023-03-01 23:01:18 +0530

    1/3 commit

--- src/component/Section.js
+++ src/component/Section.js
@@ -2,29 +2,45 @@ import React from "react";
 
 const Section = () => {
 
-    // const sectionButtons = () => {
-    //     var count = 10;
-    //     //var buttons = [];
-    //     for(let i = 0; i < count; i++){
-    //             // return (<div className="row">
-    //             //     <div className="col">
-    //             // <button className="btn btn-secondary" style={{borderRadius:'50%'}}>i</button>
-    //             // </div>
-    //             // </div>)
-    //     }
-    // }
+    const sectionButtons = () => {
+        var count = 7;
+        var buttons = [];
+        var btn = 0;
+        var col = "col-3";
+        var space = "";
+        for(let i = 0; i < count; i++){
+                for(let j=0;j<4;j++){
+                    btn = btn+ 1;
+                    if(btn<10){
+                        space = "0";
+                    }
+                    buttons.push(
+                    <div className={col}>
+                    <button className="btn btn-secondary" style={{borderRadius:'50%'}}>{space}{btn}</button>
+                    </div>);
+                    col = "col-3";
+                    space = "";
+                }
+                col = "col";
+                buttons.push(<br />);
+                buttons.push(<br />);
+                
+        }
+        return buttons;
+    }
 
     return(
         <div>
-            <div className="container" style={{width:'40%', marginTop:'-27.5%', marginLeft:'62%'}}>
+            <div className="container" style={{width:'auto', height:'auto', marginTop:'-27.5%', marginLeft:'62%'}}>
                 <div className="card m-5">
                     <div className="card-body">
                         <div className="card-title" style={{textAlign:'center'}}>
                             <h5>Section</h5>
                         </div><hr />
                         <div>
-                        <div className="row">
-                    <div className="col">
+                        <div className="row d-flex">
+                            {sectionButtons()}
+                    {/* <div className="col">
                 <button className="btn btn-secondary" style={{borderRadius:'50%'}}>1</button>
                 </div>
                 <div className="col">
@@ -33,6 +49,9 @@ const Section = () => {
                 <div className="col">
                 <button className="btn btn-secondary" style={{borderRadius:'50%'}}>3</button>
                 </div>
+                <div className="col">
+                <button className="btn btn-secondary" style={{borderRadius:'50%'}}>4</button>
+                </div> */}
                 </div>
                         </div>
                     </div>
